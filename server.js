50f5423b491da5d5e0e6752c3f5b4c2359b93c75
Jan Peter <jan.peter92@mail.de>
2022-01-26 14:23:10 +0100

    feature: server added

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/api/todos", (req, res) => {
+	res.json([{ id: 1, name: "Uwe" }]);
+});
+
 app.listen(port, () => {
 	console.log(`Test`);
 });
