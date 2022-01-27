cd2a6aefe0f9b6d76abd34a0c1f6746a074cffed
Dominik Hautau <dominikhautau@gmail.com>
2022-01-27 12:04:57 +0100

    feat: Connect to MongoDB on server startup

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import cors from "cors";
 import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 import { readFile, writeFile } from "fs/promises";
 import { v4 as uuid } from "uuid";
+import { connectDatabase } from "./utils/database.js";
+
+if (!process.env.MONGODB_URI) {
+	throw new Error("No MONGODB_URI available in dotenv");
+}
 
 const app = express();
 const port = 1337;
@@ -93,6 +102,8 @@ app.put("/api/todos", async (request, response, next) => {
 	}
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+connectDatabase(process.env.MONGODB_URI).then(() => {
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`);
+	});
 });
